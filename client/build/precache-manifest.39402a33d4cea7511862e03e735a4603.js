082683c79df8b86745623c380098e6e438040b88
miro-solution <devmiro32@gmail.com>
2023-04-28 18:59:09 +0300

    feat deploy calendar page and non appointment table

---
+++ client/build/precache-manifest.39402a33d4cea7511862e03e735a4603.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc94392cbb56aad85974f50933119554",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f23a48be5fe00bd7ea97",
+    "url": "/static/css/2.586a6272.chunk.css"
+  },
+  {
+    "revision": "e9f972a9067da303e3f7",
+    "url": "/static/css/main.12ce19ac.chunk.css"
+  },
+  {
+    "revision": "f23a48be5fe00bd7ea97",
+    "url": "/static/js/2.7d2fa8f6.chunk.js"
+  },
+  {
+    "revision": "03b1bfa9cda117f3e2c735a7ecc8615e",
+    "url": "/static/js/2.7d2fa8f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f972a9067da303e3f7",
+    "url": "/static/js/main.50c89dba.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
---
+++ client/build/precache-manifest.71be2f6e313d913bf4939b478fb13054.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf6856ba27bd6a9314227d143bfdb83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b56a5515c2ff53ff2eb4",
+    "url": "/static/css/2.586a6272.chunk.css"
+  },
+  {
+    "revision": "518e3f9565bb009cc9b3",
+    "url": "/static/css/main.12ce19ac.chunk.css"
+  },
+  {
+    "revision": "b56a5515c2ff53ff2eb4",
+    "url": "/static/js/2.54d5d9c7.chunk.js"
+  },
+  {
+    "revision": "03b1bfa9cda117f3e2c735a7ecc8615e",
+    "url": "/static/js/2.54d5d9c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "518e3f9565bb009cc9b3",
+    "url": "/static/js/main.925f853a.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
