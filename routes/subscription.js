2de424cd4221d19b56875b123d6e4a8a31fdb49d
miro-solution <devmiro32@gmail.com>
2023-04-28 17:51:29 +0300

    feat deploy calendar page and non appointment table

--- routes/subscription.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const subscriptionControllers = require('../controllers/subscriptionController');
-
-router.post('/:id', subscriptionControllers.subscription);
-
-router.delete('/unsubscribe/:id', subscriptionControllers.unsubscribe);
-
-module.exports = router;
